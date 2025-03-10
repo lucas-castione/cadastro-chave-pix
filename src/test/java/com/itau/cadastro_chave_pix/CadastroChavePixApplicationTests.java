59486d3a9076245e1cee8dfb5bcc68a71103e6c8
lucas-castione <lucas.xsilva@bandtec.com.br>
2025-03-10 16:54:39 -0300

    iniciando o projeto

---
+++ src/test/java/com/itau/cadastro_chave_pix/CadastroChavePixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.cadastro_chave_pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroChavePixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
